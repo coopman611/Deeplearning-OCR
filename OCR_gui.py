2decabb4724275b2c8be1f9714b9c951a849bff4
coopman611 <60371131+coopman611@users.noreply.github.com>
2022-10-26 11:40:13 -0400

    Add files via upload

--- OCR_gui.py
+++ OCR_gui.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog as fd
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageEnhance
 from tkinter import ttk
 
 import matplotlib.pyplot as plt
@@ -56,19 +56,39 @@ def upload():
         ).grid(row=10, column=0, sticky=W)
 
     ########## Slider work
-    current_value = tk.DoubleVar()
+    current_value = tk.IntVar()
 
     def get_current_value():
         return '{: .2f}'.format(current_value.get())
 
 
+
     def slider_changed(event):
+        
         value_label.configure(text=get_current_value())
+
+
+
+    def brightnessUpdate(e):
+        #img_for_brightness=Image.open(path1)
+        #img_brightness_obj=ImageEnhance.Brightness(img_for_brightness)
+        #factor=current_value.get()
+    
+        #enhanced_img=img_brightness_obj.enhance(factor)
+        #enhanced_img.save(path1)
+        path2="C:\\Users\\somer\\source\\repos\\OCR_gui\\tumblr_oz1pbalxNn1us9frso1_1280.jpg"
+        img2=ImageTk.PhotoImage(Image.open(path2))
+        panel.configure(image=img2)
+        panel.image=img2
+        #image_with_brightness=ttk.Label(imageWin, image=img2).grid(row=20, column=4, sticky=S)  
+        #enhanced_img.save(path1)
+
+
 
     brightness=tk.Scale(
         imageWin,
-        from_=0,
-        to=100,
+        from_=1,
+        to=10,
         orient='horizontal',
         variable=current_value,  
         command = slider_changed
@@ -96,14 +116,26 @@ def upload():
     Label(imageWin, text="Please adjust the brightness to where the writing is still visible, but any other infractions are not", bg="blue", fg="white", font="none 13 bold").grid(row=1, column=0,sticky=W)
     Label(imageWin, text="Hitting start will start the train process", bg="blue", fg="white", font="none 10").grid(row=2, column=0, sticky=W)
     start=Button(imageWin, text="Start", width=10, command=startLearning).grid(row=3, column=0, sticky=W)
+    updateBrightness=Button(imageWin, text="Update image", width=15, command=brightnessUpdate(panel)).grid(row=3, column=2, sticky=E)
     #############################displaying image chosen to be able to adjust brightness
     st=''
     #make chosen tuple a string instead 
     for item in chosen:
         st=st+item
+    global path1
     path1=st
+    global img
     img=ImageTk.PhotoImage(Image.open(path1))
-    penel=ttk.Label(imageWin, image=img).grid(row=20, column=0, sticky=S)
+    panel=ttk.Label(imageWin, image=img).grid(row=20, column=0, sticky=S)
+    
+
+    #img_for_brightness=Image.open(path1)
+    #img_brightness_obj=ImageEnhance.Brightness(img_for_brightness)
+    #factor=current_value.get()
+    
+    #enhanced_img=img_brightness_obj.enhance(factor)
+    #image_with_brightness=ttk.Label(imageWin, image=img).grid(row=20, column=0, sticky=S)
+    #enhanced_img.save(path1)
     print(img)
     imageWin.mainloop()
 
